769a192c1d54e876cfc63e1fcf35bbc08dc6527a
haseungsu07 <75104253+haseungsu07@users.noreply.github.com>
2024-12-16 18:47:06 +0900

    Update main_5.py

--- main_5.py
+++ main_5.py
@@ -1,5 +1,5 @@
 from langchain_openai import ChatOpenAI
-
+OPENAI_API_KEY = "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"
 chat_model = ChatOpenAI(api_key=OPENAI_API_KEY, model_name="gpt-3.5-turbo")
 
 st.title('AI 작사가')
