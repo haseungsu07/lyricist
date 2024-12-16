10830e1d884bcb77fc4760a39f53d3ca09bcdd9f
haseungsu07 <75104253+haseungsu07@users.noreply.github.com>
2024-12-16 18:35:11 +0900

    Update main_5.py

--- main_5.py
+++ main_5.py
@@ -1,13 +1,6 @@
 from langchain_openai import ChatOpenAI
-import streamlit as st
-import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-OPENAI_API_KEY = os.getenv("API_KEY")
-
-chat_model = ChatOpenAI(api_key=OPENAI_API_KEY, model_name="gpt-3.5-turbo")
+# chat_model = ChatOpenAI(api_key=OPENAI_API_KEY, model_name="gpt-3.5-turbo")
 
 st.title('AI 작사가')
 
